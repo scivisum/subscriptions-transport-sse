420203eee0d681691f83a6ef9604ad6261eb8140
David Feltell <david.feltell@scivisum.co.uk>
2019-06-28 16:48:02 +0100

    [RD-35280] Defer choice of native or polyfill `EventSource` to aid test mocking
    Also switch from single to double quotes.

--- src/client.js
+++ src/client.js
@@ -1,8 +1,7 @@
-import {ApolloLink, Observable} from 'apollo-link';
-import {print} from 'graphql/language/printer';
-import {NativeEventSource, EventSourcePolyfill} from 'event-source-polyfill';
+import {ApolloLink, Observable} from "apollo-link";
+import {print} from "graphql/language/printer";
+import {NativeEventSource, EventSourcePolyfill} from "event-source-polyfill";
 
-const EventSource = NativeEventSource || EventSourcePolyfill;
 
 export class SubscriptionClient {
   constructor(url) {
@@ -16,9 +15,11 @@ export class SubscriptionClient {
 
     let queryString = Object.keys(options)
       .map(key => `${encodeURIComponent(key)}=${toQueryParam(options[key])}`)
-      .join('&');
+      .join("&");
 
+    const EventSource = NativeEventSource || EventSourcePolyfill;
     const evtSource = new EventSource(`${this._url}?${queryString}`);
+    // const evtSource = this._eventSourceFactory(`${this._url}?${queryString}`);
     this._subscriptions[subID] = {options, handler, evtSource};
 
     evtSource.onmessage = event => {
@@ -27,7 +28,8 @@ export class SubscriptionClient {
     };
     evtSource.onerror = event => {
       console.warn(
-        `EventSource connection dropped for subscription ID ${subID}`, event
+        `EventSource connection dropped for subscription ID ${subID}`,
+        event
       );
       // Don't do anything other than log it, otherwise Apollo will unsubscribe when we want the
       // EventSource to auto-reconnect.
@@ -35,30 +37,30 @@ export class SubscriptionClient {
     return subID;
   }
 
-  unsubscribe(subId) {
-    if (this._subscriptions[subId] && this._subscriptions[subId].evtSource) {
-      this._subscriptions[subId].evtSource.close();
+  unsubscribe(subID) {
+    if (this._subscriptions[subID] && this._subscriptions[subID].evtSource) {
+      this._subscriptions[subID].evtSource.close();
     }
-    delete this._subscriptions[subId];
+    delete this._subscriptions[subID];
   }
 }
 
 export class SSELink extends ApolloLink {
-  constructor(paramsOrClient) {
+  constructor(subscriptionClient) {
     super();
-    this._subscriptionClient = paramsOrClient;
+    this._subscriptionClient = subscriptionClient;
   }
 
   request(operation, forward) {
     // Note: the following means that we do not support batch operations if one of them is a
     // subscription.
-    if (operation.query.definitions[0].operation !== 'subscription') {
+    if (operation.query.definitions[0].operation !== "subscription") {
       return forward(operation);
     }
     return new Observable(observer => {
       const subID = this._subscriptionClient.subscribe(
         Object.assign(operation, {query: print(operation.query)}),
-        // Odd callback signature necessary for compatibility with graphiql-_subscriptions-fetcher.
+        // Odd callback signature necessary for compatibility with graphiql-subscriptions-fetcher.
         (_, data) => {
           observer.next(data);
         }
@@ -70,7 +72,7 @@ export class SSELink extends ApolloLink {
 }
 
 const toQueryParam = obj => {
-  if (typeof obj === 'object') {
+  if (typeof obj === "object") {
     obj = JSON.stringify(obj);
   }
   return encodeURIComponent(obj);
